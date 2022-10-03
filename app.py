383116c52e17cfc8fe627b225dbb1534c3a1c3ff
Jennifer Vannier <Jen@Jennifers-MacBook-Pro.local>
2022-10-03 01:41:12 -0400

    Render frontend

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS, cross_origin
 import os
 
 
-app = Flask(__name__, static_folder='client/build',static_url_path='')
+app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
 cors = CORS(app)
 
 
@@ -18,6 +18,20 @@ cors = CORS(app)
 @cross_origin()
 def Welcome():
     return "Welcome to the API!!!"
+
+
+@app.route('/')
+def serve():
+    print("AAAAaaaanot found aaaAAA")
+    # return app.send_static_file('index.html')
+    # root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    print("not found")
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
