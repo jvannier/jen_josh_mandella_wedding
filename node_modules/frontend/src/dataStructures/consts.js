99a90106d40a4dc69573bcac6cf85a01ae533bf6
Josh <Jo.mandella@gmail.com>
2022-12-30 17:10:45 -0500

    Backend update added endpoints for rsvp, user put and get. Status added get.

---
+++ node_modules/frontend/src/dataStructures/consts.js
@@ -0,0 +1,8 @@
+// const API_URL = "https://jen-josh-mandella-wedding.herokuapp.com/";
+// const CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
+
+const API_URL = "http://localhost:3000";
+const CLIENT_ID = "..."
+
+
+export { API_URL, CLIENT_ID };
