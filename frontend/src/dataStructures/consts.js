833ae62e33efd24ce3b3291191fa7a3e9d39e9bf
Jennifer Vannier <Jen@Jennifers-MacBook-Pro.local>
2023-02-06 21:02:02 -0500

    Fix api url

--- frontend/src/dataStructures/consts.js
+++ frontend/src/dataStructures/consts.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://jen-josh-mandella-wedding.herokuapp.com";
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
 
 export { API_URL, CLIENT_ID };
